dd78780b50afe5d7d1e2442bf84bbe4935870283
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2025-02-03 18:12:21 -0800

    u

--- main.go
+++ main.go
@@ -140,7 +140,7 @@ func (ms *MasterServer) HandlePerServerToken(c *gin.Context) {
     }
 
     // Parse the RSA private key
-    privateKey, err := jwt.ParseEdPrivateKeyFromPEM(keyData)
+    privateKey, err := jwt.ParseECPrivateKeyFromPEM(keyData)
     if err != nil {
         log.Fatalf("Error parsing RSA private key: %v", err)
     }
